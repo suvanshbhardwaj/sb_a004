c05dc07824968916d037298d048706392a9cb1e7
suvanshbhardwaj <suvansh997@gmail.com>
2017-03-21 15:02:39 +0530

    Update m2.js

--- m2.js
+++ m2.js
@@ -3,7 +3,7 @@ function sayHello () {
    document.getElementById("name").value;
   if(name=="suvansh")
    var message = "<h2>Marks of " + name + "<br> Web Development= 13 <br> Computer Architechure= 12 <br> Python=7 <br> Maths=11 </h2>";
-  if(name=="samridhi")
+  else if(name=="samridhi")
     var message = "<h2>Marks of " + name + "<br> Web Development= 12 <br> Computer Architechure= 2 <br> Python=8 <br> Maths=14 </h2>";
   else
       var message = "<h2>Marks of " + name + " not avalible </h2>";
