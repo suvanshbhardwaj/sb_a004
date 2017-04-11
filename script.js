789c042c8d1b01a97290456f0293ac261aca71d8
suvanshbhardwaj <suvansh997@gmail.com>
2017-04-11 15:20:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,30 @@
-// Event handling
-document.addEventListener("DOMContentLoaded",
-  function (event) {
-    
-    // Unobtrusive event binding
-    document.querySelector("button")
-      .addEventListener("click", function () {
-        
-        // Call server to get the name
-        $ajaxUtils
-          .sendGetRequest("name.txt", 
-            function (request) {
-              var name = request.responseText;
+function loadData(){
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function(){
+		if(this.readyState == 4 && this.status==200){
+			var jsonObj = JSON.parse(this.responseText);
+			responseHandler(jsonObj);
+		}
+	};
+	function responseHandler(jsonObj){
+		var i = 0;
+		var number = document.getElementById('numberBox').value;
+		var output = document.getElementById('output');
+		var flag = 0;
+		// console.log(number);
+		for (var i = 0; i < jsonObj.length; i++) {
+			if (number == jsonObj[i].number) {
+				output.innerHTML = jsonObj[i].name;
+				flag = 1;
+				break;
+			}
+		}
+		if (flag == 0) {
+			output.innerHTML = "<span style='color:red;'>Number could not be found</span>";
+		}
+	}
+	xhttp.open("GET","data.json",true);
+	xhttp.send();	
+}
 
-              document.querySelector("#content")
-                .innerHTML = "<h2>Hello " + name + "!</h2>";
-            });
 
-        
-      });
-  }
-);
